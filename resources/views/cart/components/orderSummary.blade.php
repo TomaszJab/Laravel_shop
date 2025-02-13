aaf2f6a7b036c40cc29f099cd51b06c51d647029
tomek.j.jablonski@gmail.com <tomek.j.jablonski@gmail.com>
2025-02-13 21:29:51 +0100

    change show.blade

--- resources/views/cart/components/orderSummary.blade.php
+++ resources/views/cart/components/orderSummary.blade.php
@@ -1,18 +1,32 @@
 <h5 class="card-title mb-4">Order Summary</h5>
-                <div class="d-flex justify-content-between mb-3">
-                    <span>Subtotal</span>
-                    <span>$199.97</span>
-                </div>
-                <div class="d-flex justify-content-between mb-3">
-                    <span>Shipping</span>
-                    <span id="payment">$0.00</span>
-                </div>
-                <div class="d-flex justify-content-between mb-3">
-                    <span>Tax</span>
-                    <span id="price">$25.00</span>
-                </div>
-                <hr>
-                <div class="d-flex justify-content-between mb-4">
-                    <strong>Total</strong>
-                    <strong>${{$total}}</strong>
-                </div>
+<div class="d-flex justify-content-between mb-3">
+    <span>Subtotal</span>
+    <span id="subtotalValue">${{$subtotal}}</span>
+</div>
+<div class="d-flex justify-content-between mb-3">
+    <span>Shipping</span>
+    <span id="shippingValue">${{$shipping}}</span>
+</div>
+<div class="d-flex justify-content-between mb-3">
+    <span>Payment</span>
+    <span id="paymentValue">${{$payment}}</span>
+</div>
+@if (!empty($cart['promo_code']))
+<div class="discount" id="discount" style="display: inline;">
+@else
+<div class="discount" id="discount" style="display: none;">
+@endif
+    <div class="d-flex justify-content-between mb-3">
+        <span>Discount</span>
+        <span id="discountValue">{{$cart['promo_code']}}%</span>
+    </div> 
+</div>    
+<hr>
+<div class="d-flex justify-content-between mb-4">
+    <strong>Total</strong>
+    @if ($cart['promo_code']<>'')
+    <strong id="totalValue">${{$total - ($total * $cart['promo_code'])/100}}</strong>
+    @else
+    <strong id="totalValue">${{$total}}</strong>
+    @endif
+</div>
--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -80,39 +80,9 @@
         <!-- Cart Summary -->
         <div class="card cart-summary" style="background-color: #f8f9fa;">
             <div class="card-body">
-                <h5 class="card-title mb-4">Order Summary</h5>
-                <div class="d-flex justify-content-between mb-3">
-                    <span>Subtotal</span>
-                    <span id="subtotalValue">${{$subtotal}}</span>
-                </div>
-                <div class="d-flex justify-content-between mb-3">
-                    <span>Shipping</span>
-                    <span id="shippingValue">${{$shipping}}</span>
-                </div>
-                <div class="d-flex justify-content-between mb-3">
-                    <span>Payment</span>
-                    <span id="paymentValue">${{$payment}}</span>
-                </div>
-                @if (!empty($cart['promo_code']))
-                <div class="discount" id="discount" style="display: inline;">
-                @else
-                <div class="discount" id="discount" style="display: none;">
-                @endif
-                    <div class="d-flex justify-content-between mb-3">
-                        <span>Discount</span>
-                        <span id="discountValue">{{$cart['promo_code']}}%</span>
-                    </div> 
-                </div>
-                              
-                <hr>
-                <div class="d-flex justify-content-between mb-4">
-                    <strong>Total</strong>
-                @if ($cart['promo_code']<>'')
-                    <strong id="totalValue">${{$total - ($total * $cart['promo_code'])/100}}</strong>
-                @else
-                    <strong id="totalValue">${{$total}}</strong>
-                @endif
-                </div>
+
+            @include('cart.components.orderSummary')
+
                 <a href="{{ route('carts.delivery') }}" class="btn btn-primary w-100">Delivery and payment</a>
             </div>
         </div>
